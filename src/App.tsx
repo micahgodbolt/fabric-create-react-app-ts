f9aff86c5d23ac3e1958eb2f1b1e387d8a4f621d
Micah Godbolt <mgodbolt@microsoft.com>
2017-10-24 16:25:58 -0700

    step2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { Fabric, TextField } from 'office-ui-fabric-react';
+import { Fabric, TextField, PrimaryButton } from 'office-ui-fabric-react';
 
 interface ITodoState {
   text?: any;
@@ -15,6 +15,12 @@ class App extends React.Component<any, ITodoState> {
         <TextField
           label="Enter Todo"
           onChanged={(text) =>  this.setState({ text })}
+        />
+
+        <PrimaryButton
+          iconProps={{iconName: 'Add'}}
+          text="Add Todo"
+          onClick={() => alert(this.state.text)}
         />
 
       </Fabric>
