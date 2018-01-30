7aa284c7d716be3e90aa98370306d6e8bfe35563
Micah Godbolt <mgodbolt@microsoft.com>
2018-01-30 08:58:32 -0800

    Updated versions, added button example

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 
-import { Fabric } from 'office-ui-fabric-react';
+import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 
 class App extends React.Component<any, any> {
 
   render() {
     return (
       <Fabric className="App">
-        Hi
+        <DefaultButton onClick={() => alert('clicked')} > Hi </DefaultButton>
       </Fabric>
     );
   }
